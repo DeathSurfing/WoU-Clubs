0d2aa2ef3c369a749a6c71ab286f118b553a75bf
Aditya Vikram Mahendru <70932037+DeathSurfing@users.noreply.github.com>
2025-04-04 18:44:46 +0530

    Update mongo.ts

--- NextApp/lib/mongo.ts
+++ NextApp/lib/mongo.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "";
+// const MONGODB_URI = process.env.MONGODB_URI || "";
 
-if (!MONGODB_URI) {
-  throw new Error("⚠️ MongoDB URI is missing in .env file");
-}
+// if (!MONGODB_URI) {
+//   throw new Error("⚠️ MongoDB URI is missing in .env file");
+// }
 
-export const connectToDatabase = async () => {
-  if (mongoose.connection.readyState >= 1) {
-    return;
-  }
-  await mongoose.connect(MONGODB_URI);
-  console.log("✅ Connected to MongoDB");
-};
+// export const connectToDatabase = async () => {
+//   if (mongoose.connection.readyState >= 1) {
+//     return;
+//   }
+//   await mongoose.connect(MONGODB_URI);
+//   console.log("✅ Connected to MongoDB");
+// };
