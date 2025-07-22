ea606940a0b212d32b0c6442eb73999df3ba131b
Aditya Vikram Mahendru <70932037+DeathSurfing@users.noreply.github.com>
2025-07-22 23:34:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,22 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
