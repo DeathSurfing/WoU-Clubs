c088870cebad41d482a2ac1f08db3a71a89fd4bf
Aditya Vikram Mahendru <70932037+DeathSurfing@users.noreply.github.com>
2025-08-19 15:00:10 +0530

    tugofwarevent

--- data/events.ts
+++ data/events.ts
@@ -1,6 +1,6 @@
 export const eventsData = [
   {
-    "id": "472025-165311",
+    "id": "1",
     "title": "Interviews for Senior Executive Position",
     "description": "Interviews for the Senior Executive Position are being conducted to identify visionary leaders with strategic acumen and a proven track record. Candidates will be evaluated on leadership skills, decision-making ability, and alignment with the university's mission.",
     "startDate": "2025-04-06",
@@ -12,5 +12,20 @@ export const eventsData = [
     "image": "/SCLogoDark.webp",
     "registerUrl": "/",
     "isFeatured": true
-  }
+  },
+  {
+  "id": "2",
+  "title": "💪 Tug of War – Show Your Strength, Show Your Spirit!",
+  "description": "Are you ready to pull, fight with grit, and claim victory? Join us for an epic Tug of War where strength meets strategy and teamwork decides the winner! Multiple exciting rounds with twists, subgroup challenges, and a grand final showdown await!",
+  "startDate": "2025-08-21",
+  "startTime": "17:00",
+  "endTime": "20:00",
+  "location": "Admin Lawn",
+  "category": "Sports & Gaming",
+  "clubId": "studentcouncil",
+  "image": "/events/tow.webp",
+  "registerUrl": "https://forms.gle/wdWkbnBdaWBXURQZA",
+  "isFeatured": true
+}
+
 ];
